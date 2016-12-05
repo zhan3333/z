40d77cd7ad1305e85614018fd63b2c485a890aa6
zhanguang <zhanguang@ykxing.com>
2016-12-05 17:39:57 +0800

    文件管理系统配置

--- src/Config/fileSystem.php
+++ src/Config/fileSystem.php
@@ -6,8 +6,7 @@
  * Time: 20:54
  */
 $uploadDir = 'test';
-$downBaseUrl = 'http://res.zhannnnn.top/work/z/';
 return [
     'uploadDir' => $uploadDir,
-    'downBaseUrl' => $downBaseUrl
+    'rootPath' => APPPATH . '/' . $uploadDir
 ];

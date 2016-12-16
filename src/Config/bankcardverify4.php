f2795d074294cfc1fae78d5dbe5936f7a4f56a7f
zhanguang <zhanguang@ykxing.com>
2016-12-16 10:03:51 +0800

    新增银行卡4要素api配置；

---
+++ src/Config/bankcardverify4.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 银行卡4元素认证配置文件
+ * doc: https://market.aliyun.com/products/57000002/cmapi011455.html#sku=yuncode545500005
+ * User: zhan
+ * Date: 2016/12/16
+ * Time: 9:57
+ */
+$conf = [
+    // 简单调用（使用appcode调用）
+    'simple' => [
+        'appcode' => ''
+    ],
+    // api签名调用
+    'signature' => []
+];
+return $conf;
